b78b2423f04cd1425c3fcef044f7e68c469ec993
Trantorian <eliot.code@proton.me>
2024-02-08 09:21:16 +0000

    fix(genesis): :bug: genesis block data not being downloaded
    Turns out we just did +1 on the first block lol

--- crates/client/deoxys/src/lib.rs
+++ crates/client/deoxys/src/lib.rs
@@ -14,7 +14,7 @@ pub mod m;
 type CommandSink = futures::channel::mpsc::Sender<sc_consensus_manual_seal::rpc::EngineCommand<sp_core::H256>>;
 
 pub async fn sync(sender_config: SenderConfig, fetch_config: FetchConfig, rpc_port: u16, l1_url: Url) {
-    let first_block = utility::get_last_synced_block(rpc_port).await + 1;
+    let first_block = utility::get_last_synced_block(rpc_port).await;
 
     let _ = join!(l1::sync(l1_url, rpc_port), l2::sync(sender_config, fetch_config, first_block, rpc_port));
 }
